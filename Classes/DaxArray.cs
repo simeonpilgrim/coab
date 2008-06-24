438c7235ab20f8b28058ecc2a8c41e73e96d31d5
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2008-06-24 01:43:42 +0000

    Fixed crash, caused by trying to load the 8 frame Medusa animation

--- Classes/DaxArray.cs
+++ Classes/DaxArray.cs
@@ -14,12 +14,15 @@ namespace Classes
         {
             numFrames = 0;
             curFrame = 0;
-            ptrs = new Struct_1D558[5];
-            ptrs[0] = new Struct_1D558(); // 1D558
-            ptrs[1] = new Struct_1D558(); // 1D55C
-            ptrs[2] = new Struct_1D558(); // 1D560
-            ptrs[3] = new Struct_1D558(); // 1D564
-            ptrs[4] = new Struct_1D558(); // 1D568
+            ptrs = new Struct_1D558[8];
+            ptrs[0] = new Struct_1D558(); // 1D558 - seg600:7248
+            ptrs[1] = new Struct_1D558(); // 1D560 - seg600:7250
+            ptrs[2] = new Struct_1D558(); // 1D568 - seg600:7258
+            ptrs[3] = new Struct_1D558(); // 1D570 - seg600:7260
+            ptrs[4] = new Struct_1D558(); // 1D578 - seg600:7268
+            ptrs[5] = new Struct_1D558(); // 1D580 - seg600:7270
+            ptrs[6] = new Struct_1D558(); // 1D588 - seg600:7278
+            ptrs[7] = new Struct_1D558(); // 1D590 - seg600:7280
         }
     }
 }
