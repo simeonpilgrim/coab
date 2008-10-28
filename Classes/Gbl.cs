310828c102d7b4eb7f503378292bef50d031a928
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2008-10-28 10:28:48 +0000

    Fixed the initializing of gbl.setBlocks

--- Classes/Gbl.cs
+++ Classes/Gbl.cs
@@ -298,6 +298,8 @@ namespace Classes
 
         public class SetBlock
         {
+            public SetBlock() { setId = -1; blockId = -1; }
+            public SetBlock(int _setId, int _blockId) { setId = _setId; blockId = _blockId; }
             public int blockId; // byte_1D53A[di] 1*4
             public int setId; // byte_1D53C[di] 1*4
         }
