8a0b6fb0e395e82176f90567f36d6d0c23d7eb48
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2010-09-28 14:39:34 +0000

    Tidy-up after reviewing of James Beck's PPC port of the project.

--- Main/Settings.cs
+++ Main/Settings.cs
@@ -9,20 +9,6 @@ namespace Main.Properties {
     internal sealed partial class Settings {
         
         public Settings() {
-            // // To add event handlers for saving and changing settings, uncomment the lines below:
-            //
-            // this.SettingChanging += this.SettingChangingEventHandler;
-            //
-            // this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
-        }
-        
-        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
-            // Add code to handle the SettingChangingEvent event here.
-        }
-        
-        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
-            // Add code to handle the SettingsSaving event here.
         }
     }
 }
