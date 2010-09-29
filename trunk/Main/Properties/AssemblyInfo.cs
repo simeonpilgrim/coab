8c498ee8e788362d040a78eafac25ca7432d3318
simeon.pilgrim <simeon.pilgrim@5a46be3a-3a1e-0410-be2a-b73053c84cf5>
2010-09-29 20:47:35 +0000

    Release 1.1.4

--- trunk/Main/Properties/AssemblyInfo.cs
+++ trunk/Main/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.4")]
