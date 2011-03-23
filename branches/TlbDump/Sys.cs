a9a733891bd195f61dff7e281d8649f1522191b1
simeon.pilgrim@gmail.com <simeon.pilgrim@gmail.com@5a46be3a-3a1e-0410-be2a-b73053c84cf5>
2011-03-23 18:43:42 +0000

    Add TlbDump project to repository

---
+++ branches/TlbDump/Sys.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TlbDump
+{
+    class Sys
+    {
+        private Sys()
+        {
+        }
+
+        public static int ArrayToInt(byte[] data, int offset)
+        {
+            int i = data[offset + 0] + (data[offset + 1] << 8) + (data[offset + 2] << 16) + (data[offset + 3] << 24);
+            return i;
+        }
+
+        public static uint ArrayToUint(byte[] data, int offset)
+        {
+            uint i = (uint)(data[offset + 0] + (data[offset + 1] << 8) + (data[offset + 2] << 16) + (data[offset + 3] << 24));
+            return i;
+        }
+
+        public static short ArrayToShort(byte[] data, int offset)
+        {
+            short i = (short)(data[offset + 0] + (data[offset + 1] << 8));
+            return i;
+        }
+
+        public static ushort ArrayToUshort(byte[] data, int offset)
+        {
+            ushort i = (ushort)(data[offset + 0] + (data[offset + 1] << 8));
+            return i;
+        }
+    }
+}
