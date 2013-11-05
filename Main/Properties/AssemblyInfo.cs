8f9d0eee6629f592e966bd41ca5d7a976c9bbcca
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2013-11-05 20:53:22 +0000

    - Release 1.1.7

--- Main/Properties/AssemblyInfo.cs
+++ Main/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.7")]
