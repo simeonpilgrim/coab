35e81afdf697eda3a148bd4084fb0284427f5200
Robert Snyder <immeraufdemhund@hotmail.com>
2016-05-20 05:20:46 -0500

    Adding in some text about file formats

---
+++ GoldBox.Data/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GoldBox.Data
+{
+    public static class Extensions
+    {
+        public static byte[] ToByteArray<T>(T dataStructure) where T : struct
+        {
+            int size = Marshal.SizeOf(dataStructure);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(dataStructure, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
+        public static T MarshalAs<T>(byte[] rawDataStructure) where T : struct
+        {
+            var type = typeof(T);
+            int size = Marshal.SizeOf(type);
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.Copy(rawDataStructure, 0, ptr, size);
+            T structure = (T)Marshal.PtrToStructure(ptr, type);
+            Marshal.FreeHGlobal(ptr);
+
+            return structure;
+        }
+    }
+}
---
+++ GoldBox.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GoldBox.Data")]
+[assembly: AssemblyProduct("GoldBox.Data")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
